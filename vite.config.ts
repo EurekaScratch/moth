4c7791adf56d43b6d554e0d4b6d1e7a2ba2731d5
SimonShiki <sinangentoo@gmail.com>
2023-10-26 15:26:04 +0000

    :zap: chore: add image optimization
    Signed-off-by: SimonShiki <sinangentoo@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import solid from 'vite-plugin-solid';
+import { ViteImageOptimizer as optimizer } from 'vite-plugin-image-optimizer';
 import suid from "@suid/vite-plugin";
 
 export default defineConfig({
-    plugins: [suid(), solid()]
+    plugins: [suid(), solid(), optimizer()]
 });
